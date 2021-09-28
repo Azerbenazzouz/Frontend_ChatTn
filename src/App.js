2383fd6a6767e3cb887c94bcbc3c236b9d1d6174
azer <benazzouzaze@gmail.com>
2021-09-28 20:33:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         Welcome to Chat Tn
       </header>
     </div>

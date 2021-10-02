bd5a9d0cebaceda1c97b1a64b5c7a28a6515c138
azer <benazzouzaze@gmail.com>
2021-10-02 16:18:30 +0100

    first commit

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import './LandingPage.css'
+import img from './1.png'
+import logo from './logo.png'
+
 function LandingPage() {
     return (
         <div className='LandingPage'>
-            <h1>Chat Tn</h1>
+
+        <div className='row pt-3'>
+                <span className="col-1"></span>
+                <span className=' logo col-1'>
+                    <img src={logo} className='logo col'/>
+                </span>
+            </div>
+
+            <div className="row">
+                <span className="col-2"></span>
+                <span className="col-3"><img className='img' src={img} alt=""/></span>
+                <span className="col-1"></span>
+                <span className="col-1">
+                    <h1 className='title'>Chat and enjoy</h1>
+                    <h4 className='sub_title'>Free and secure and 100% made in Tunisia</h4>
+                    <span className='btn btn-primary mt-5'>Get Started</span>
+                </span>
+            </div>
         </div>
     )
 }

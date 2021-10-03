31290b6b8f5bcd45647f1cae6f8408b0750372e5
azer <benazzouzaze@gmail.com>
2021-10-03 21:53:16 +0100

    💙💚first commit❤🔥

--- src/components/GetStarted/GetStarted.js
+++ src/components/GetStarted/GetStarted.js
@@ -5,7 +5,19 @@ function GetStarted() {
     return (
         <div className="GetStarted">
             <center>JOIN THE <span className="fun">FUN.</span></center>
+            <div className="row">
+                <span className="col-1"></span>
+                <div className="col-4">
+                    
+                </div>
+                <spam className="col-2"></spam>
+                <center className="or">OR</center>
+                <div className="col-4">
+                    
+                </div>
+            </div>
         </div>
+        
     )
 }
 

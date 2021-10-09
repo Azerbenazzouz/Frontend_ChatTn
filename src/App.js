e5d190484f3f70790da040ffd0fa609ae16c1e5d
azer <benazzouzaze@gmail.com>
2021-10-09 14:53:13 +0100

    ❤Fix LandingPage css 🔥🤤

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import GetStarted from './components/GetStarted/GetStarted';
-import LandingPage2 from './components/LandingPage/LandingPage2'
+import LandingPage from './components/LandingPage/LandingPage'
 import { Route, Switch , BrowserRouter } from 'react-router-dom'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="">
         <BrowserRouter>
           <Switch>
-                  <Route exact path="/" component={LandingPage2} />
+                  <Route exact path="/" component={LandingPage} />
                   <Route path="/GetStarted" component={GetStarted} />
           </Switch>
         </BrowserRouter>
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,32 +1,24 @@
 import React from 'react'
 import './LandingPage.css'
 import img from './asset/1.png'
-import logo from './asset/logo.png'
 import { Link } from 'react-router-dom'
 
 function LandingPage() {
     return (
-        <div className='LandingPage'>
-
-        <div className='row pt-3'>
-            <span className="col-1"></span>
-            <span className=' logo col-1'>
-                <img src={logo} alt='' className='logo col'/>
-            </span>
-        </div>
-
-            <div className="row">
-                <span className="col-2"></span>
-                <span className="col-3"><img className='img' src={img} alt=""/></span>
-                <span className="col-1"></span>
-                <span className="col-1">
-                    <h1 className='title'>Chat and enjoy</h1>
-                    <h4 className='sub_title'>Free and secure and 100% made in Tunisia</h4>
-                    
-                    <Link to='/GetStarted'>
-                        <span className='btn btn-primary mt-5'>Get Started</span>
-                    </Link>
-                </span>
+        <div className='LandingPage '>
+            <div className="r">
+                <div className="r1">
+                <img className='img' src={img} alt=""/>
+                </div>
+                <div className="r2">
+                    <div className="c">
+                        <h1 className='title'>Chat and enjoy</h1>
+                        <h4 className='sub_title'>Free and secure and 100% made in Tunisia</h4>
+                        <Link to='/GetStarted'>
+                            <span className='btn btn-primary mt-5'>Get Started</span>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     )

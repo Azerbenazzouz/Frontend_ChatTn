d92a79fa40235068e5ede03473dfd4d65a3e89f7
azer <benazzouzaze@gmail.com>
2021-10-09 14:46:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import GetStarted from './components/GetStarted/GetStarted';
-import LandingPage from './components/LandingPage/LandingPage'
+import LandingPage2 from './components/LandingPage/LandingPage2'
 import { Route, Switch , BrowserRouter } from 'react-router-dom'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="">
         <BrowserRouter>
           <Switch>
-                  <Route exact path="/" component={LandingPage} />
+                  <Route exact path="/" component={LandingPage2} />
                   <Route path="/GetStarted" component={GetStarted} />
           </Switch>
         </BrowserRouter>
--- src/components/GetStarted/GetStarted.js
+++ src/components/GetStarted/GetStarted.js
@@ -3,18 +3,35 @@ import './GetStarted.css'
 
 function GetStarted() {
     return (
-        <div className="GetStarted">
-            <center>JOIN THE <span className="fun">FUN.</span></center>
+        <div className="GetStarted ">
+            <center className="title_JOIN">JOIN THE <span className="fun">FUN.</span></center>
             <div className="row">
                 <span className="col-1"></span>
-                <div className="col-4">
+                <div className="col-4 frame">
                     
+                    <div className="ms-5 mt-5 login">
+                        <h1 className="frame_text pt-4 pb-4">
+                            Email :
+                        </h1>
+                        <input type="email" placeholder="Email" className="mb-5 input"/>
+                        <h1 className="frame_text pb-4">
+                            Password :
+                        </h1>
+                        <input type="password" placeholder="Password" className="mb-5 input"/>
+                        <span className='btn btn-primary mt-3'>Get Started</span>
+                    </div>
+
                 </div>
+                
                 <spam className="col-2"></spam>
-                <center className="or">OR</center>
-                <div className="col-4">
-                    
+                <div className="col-4 frame">
+                    <div className="register">
+                        <div className="ms-5 mt-5 ">
+                            
+                        </div>
+                    </div>
                 </div>
+                <span className="col-1"></span>
             </div>
         </div>
         

8cd3f9d97b0d7ea61863f654ff06ca8b74a00afd
azer <benazzouzaze@gmail.com>
2021-10-19 07:00:30 +0100

    ❤FrontEnd Chat Tn🌌

---
+++ src/components/Home/components/MsgHome.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { PersonFill,List} from 'react-bootstrap-icons';
+import './MsgHome.css'
+function MsgHome({Img,Name,Date,Lmsg}) {
+    return (
+        <div>
+            <div className="MsgHome">
+                <div className="ImgAndInfoMsgHome">
+                    {Img ? <img src={Img} alt="mdo" width="60" height="60" class="rounded-circle"/>:<PersonFill size={60}/>}
+                    
+                    <div style={{paddingLeft:"2vw"}}>
+                        <h4>{Name}</h4>
+                        <h5 style={{color:"gray",width:"60vw",paddingLeft:"1vw"}}>{Lmsg}<span style={{paddingLeft:"1vw"}}>{Date}</span></h5>
+                    </div>
+                </div>
+                <List size={60}/>
+            </div>
+        </div>
+    )
+}
+
+export default MsgHome

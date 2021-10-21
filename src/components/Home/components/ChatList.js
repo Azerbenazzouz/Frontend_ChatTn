647e0a0ac9f6fdf0a3921dcc0d242ded42343d3a
azer <benazzouzaze@gmail.com>
2021-10-21 08:59:29 +0100

    ❤FrontEnd Chat Tn🌌

--- src/components/Home/components/ChatList.js
+++ src/components/Home/components/ChatList.js
@@ -10,6 +10,7 @@ function ChatList({Msg,setMsg}) {
     const email = useSelector(state => state.email)
     const refreshToken = useSelector(state => state.refreshToken)
     const [Data,setData]=useState([])
+    const [DataP2P,setDataP2P]=useState([])
 
     const GetGroups =async ( ) =>{
         var config = {
@@ -29,8 +30,27 @@ function ChatList({Msg,setMsg}) {
         })
     }
 
+
+    const GetP2P =async ( ) =>{
+        var config = {
+            method: 'get',
+            url: process.env.REACT_APP_URL+'/messenger/p2p/GetAllP2P',
+            headers: { 
+                'x-api-key': process.env.REACT_APP_X_API_KEY, 
+                'Authorization': 'Bearer '+refreshToken, 
+                'Content-Type': 'application/json'
+            },
+            data : {email: email}
+        };
+    
+        await axios(config)
+        .then(function (response) {
+            setDataP2P(response.data)
+        })
+    }
     useEffect(()=>{
         GetGroups()
+        GetP2P()
     })
 
     return (
@@ -40,7 +60,25 @@ function ChatList({Msg,setMsg}) {
                 <Button variant="success" className="BtnSwitch" active={!Msg===true ?  true : false} onClick={()=>setMsg(false)} >Groups</Button>
             </ButtonGroup>
             {
-                Data.length===0 ? <h1 style={{color: 'white',width:"90vw",textAlign: 'center'}}>No {Msg ? "messages":"groups"}</h1>:Data?.map((data)=>{
+                Msg ? DataP2P.length===0 ? <h1 style={{color: 'white',width:"90vw",textAlign: 'center'}}>No {Msg ? "messages":"groups"}</h1>:DataP2P?.map((data)=>{
+
+                    var DateN = new Date(data.lastTime)
+                    var date = DateN.getDate()+"/"+DateN.getMonth()+"/"+DateN.getFullYear()+"  "
+                    var time1 =new Date(Date.now()-DateN).getHours()+" h"
+                    var time2 =new Date(Date.now()-DateN).getMinutes()+" min"
+                    var timeT =""
+
+                    if(date !== (new Date(Date()).getDate()+"/"+ new Date(Date()).getMonth()+"/"+new Date(Date()).getFullYear()+"  ")){
+                        timeT=date
+                    }else if(new Date(Date.now()-DateN).getHours()>1){
+                        timeT=time1
+                    }else{
+                        timeT=time2
+                    }
+
+                    return <MsgHome Img={data.img} Name={data.name} Date={timeT} Lmsg={data.lastMessage ? data.lastMessage : "New Group"} GrpId={data._id} key={data._id} />
+                    
+                }) : Data.length===0 ? <h1 style={{color: 'white',width:"90vw",textAlign: 'center'}}>No {Msg ? "messages":"groups"}</h1>:Data?.map((data)=>{
 
                     var DateN = new Date(data.lastTime)
                     var date = DateN.getDate()+"/"+DateN.getMonth()+"/"+DateN.getFullYear()+"  "
